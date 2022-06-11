b504662ba18c56dec1ebd3d8aec062da7fbf5e43
Michael <michael.spengler@sap.com>
2022-06-11 12:01:48 +0200

    draft the welcome-gift feature

---
+++ cult/README.md
@@ -0,0 +1,25 @@
+# CultDAO Welcome Gift
+
+## For Users
+
+## For Developers
+Contributions are welcome. To simplify contributions I describe how this project was created.  
+
+The UI is created using [svelte](https://svelte.dev/docs).
+
+```sh
+
+npx degit sveltejs/template welcome-gift
+cd welcome gift
+node scripts/setupTypeScript.js # ... to use TypeScript
+npm i 
+
+```
+
+You can work on and test the UI by executing the following statement within the welcome-gift folder.
+
+```sh
+
+npm run dev
+
+```
---
+++ cult/welcome-gift/src/App.svelte
@@ -0,0 +1,143 @@
+<script lang="ts">
+	let walletAddress = "";
+	let socialMediaProfileLink = "";
+	let visitorLevel = 0;
+	function confirmData() {
+		alert("confirmed");
+	}
+</script>
+
+<main>
+	<video autoplay muted loop>
+		<source
+			src="https://cultdao.io/wp-content/uploads/2022/01/Stormy-Sky2.mp4"
+			type="video/mp4"
+		/>
+	</video>
+
+	<div id="overlay">
+		<h1>CultDAO Ecosystem</h1>
+		<h1>Welcome Present</h1>
+		<p>
+			Some of us are pretty lucky as we could explore the distributed
+			ledger space from early on.
+		</p>
+		<p>
+			So some of us have strong wallets containing many valuable Cult
+			Tokens.
+		</p>
+		<p>
+			Some of us want to give newcomers the chance to get familiar with
+			<a href="https://metamask.io" target="_blank">metamask.io</a> and are
+			ready to donate some Cult to those newcomers.
+		</p>
+
+		<p><br /></p>
+
+		{#if visitorLevel === 0}
+			<button on:click={() => confirmData()}> I'm a newcomer </button>
+			<button on:click={() => confirmData()}> I'm an insider </button>
+			<br />
+		{/if}
+
+		{#if visitorLevel == 1}
+			If you are a newcomer feel free to ...
+			<p><br /></p>
+
+			... enter your wallet address in which you want to receive CULT
+			<input bind:value={walletAddress} />
+
+			<p><br /></p>
+			... enter the link to your social media profile (e.g. your facebook profile).
+			<input bind:value={socialMediaProfileLink} />
+
+			<p><br /></p>
+			... post the following statement on your social media profile "I like
+			the https://cultdao.io and I am ready to receive a welcome present from
+			https://peer-2-peer.eth.link"
+
+			<p><br /></p>
+
+			{#if socialMediaProfileLink != ""}
+				Your Social Media Profile Link: <a
+					href={socialMediaProfileLink}
+					target="_blank">{socialMediaProfileLink}</a
+				> <br />
+			{/if}
+
+			{#if walletAddress != ""}
+				Your Wallet Address: {walletAddress} <br />
+			{/if}
+
+			<p><br /></p>
+			{#if walletAddress != "" && socialMediaProfileLink != ""}
+				<button on:click={() => confirmData()}> That's Correct </button>
+			{/if}
+		{/if}
+
+		<p><br /></p>
+		Here is a list of newcomers. As soon as they posted the following statements
+		to the CultDAO publicly on their profile, everyone who likes them, can send
+		them some Cult.
+	</div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+		color: white;
+	}
+
+	h1 {
+		color: turquoise;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+
+	video {
+		z-index: 2;
+	}
+
+	#overlay {
+		position: fixed; /* Sit on top of the page content */
+		width: 100%; /* Full width (cover the whole page) */
+		height: 100%; /* Full height (cover the whole page) */
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		background-color: rgba(
+			0,
+			0,
+			0,
+			0.5
+		); /* Black background with opacity */
+		z-index: 1; /* Specify a stack order in case you're using a different order for other elements */
+		cursor: pointer; /* Add a pointer on hover */
+	}
+	/* 
+	.home {
+		height: 100vh;
+		position: relative;
+	}
+
+	.overlay {
+		position: absolute;
+		top: 0;
+		left: 0;
+		bottom: 0;
+		right: 0;
+		z-index: 2;
+		background: rgba(0, 0, 0, 0.6);
+	} */
+</style>
