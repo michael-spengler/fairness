5482307f5c7fddf6283eb403e4d107217a7eda46
Michael <michael.spengler@sap.com>
2022-06-16 13:52:57 +0200

    add connect via browserwallet feature

--- nft-platform-which-only-accepts-cult-for-payments/client/src/App.svelte
+++ nft-platform-which-only-accepts-cult-for-payments/client/src/App.svelte
@@ -1,25 +1,66 @@
 <script lang="ts">
-	
+    import Connect from "./Connect.svelte";
+
+    export let pwaName;
+    let mode = 0;
+    let account = "";
+    let provider;
+
+    function wantToBuyNFTs() {
+        mode = 1;
+    }
+
+    function wantToSellNFTs() {
+        mode = 2;
+    }
 </script>
 
 <main>
-	<video autoplay muted loop id="myVideo">
-		<source
-			src="https://cultdao.io/wp-content/uploads/2022/01/Stormy-Sky2.mp4"
-			type="video/mp4"
-		/>
-	</video>
+    <video autoplay muted loop id="myVideo">
+        <source
+            src="https://cultdao.io/wp-content/uploads/2022/01/Stormy-Sky2.mp4"
+            type="video/mp4"
+        />
+    </video>
 
-	<div class=content>
+    <div class="content">
+        <h1>{pwaName}</h1>
+        <h2>
+            Here you can buy and sell NFTs using <a
+                href="https://coinmarketcap.com/currencies/cult-dao/"
+                target="_blank">CULT</a
+            >
+        </h2>
 
-		<h1>THE NFT CULT</h1>
-		<h2>Here you can buy and sell NFTs using <a href="https://coinmarketcap.com/currencies/cult-dao/" target="_blank">CULT</a></h2>
-	</div>
+        <p><br /></p>
 
+        <Connect bind:account bind:provider />
+        {#if account !== ""}
+            {#if mode === 0}
+                <button on:click={() => wantToBuyNFTs()}>Buy NFTs</button>
+                <button on:click={() => wantToSellNFTs()}>Sell NFTs</button>
+            {/if}
+
+            {#if mode === 1}
+                <h3>
+                    NFT Gallery
+                    ... under construction ...
+                </h3> 
+                {/if}
+                
+                {#if mode === 2}
+                <h3>
+                    Offer Your NFTs
+                    ... under construction ...
+                </h3> 
+
+            {/if}
+        {/if}
+    </div>
 </main>
 
 <style>
-	 a {
+    a {
         color: red;
     }
     #myVideo {
@@ -53,6 +94,12 @@
         color: turquoise;
         text-transform: uppercase;
         font-size: 3em;
+        font-weight: 100;
+    }
+    h3 {
+        color: turquoise;
+        text-transform: uppercase;
+        font-size: 2em;
         font-weight: 100;
     }
 
---
+++ nft-platform-which-only-accepts-cult-for-payments/client/src/Connect.svelte
@@ -0,0 +1,35 @@
+<script>
+    import { ethers } from "ethers";
+    export let account = "";
+    export let provider = "";
+    async function connectToBrowserWallet() {
+        if (typeof window.ethereum === "undefined") {
+            alert("You need to install a browserwallet like metamask.io.");
+        } else {
+            const accounts = await ethereum.request({
+                method: "eth_requestAccounts",
+            });
+            account = accounts[0];
+            provider = new ethers.providers.Web3Provider(
+                window.ethereum,
+                "any"
+            );
+
+        }
+    }
+</script>
+
+<div >
+    {#if account !== ""}
+        You are logged in via the following browserwallet: <p />
+       <h4 class="account"> {account}</h4> <p>on chainID {provider.provider.chainId}.</p>
+    {:else}
+        <button on:click={connectToBrowserWallet} class="btn btn--primary">Connect Browserwallet</button>
+    {/if}
+</div>
+
+<style>
+    .account {
+       margin-top: 0;
+    }
+</style>
